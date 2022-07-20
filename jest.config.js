0bb91d41da04cb5797032ee21afdc7de5cc2d22a
Luiz Eduardo Junior <92542169+Luiz-Eduardo-Gaivota@users.noreply.github.com>
2022-07-20 16:44:00 -0300

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -16,11 +16,5 @@ module.exports = {
   testMatch: ["**/*.test.(ts|tsx)"],
   reporters: ["default"],
   coverageReporters: ["json", "lcov", "text"],
-  moduleNameMapper: {
-    // Mocks out all these file formats when tests are run
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "identity-obj-proxy",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
   collectCoverage: true,
 };
