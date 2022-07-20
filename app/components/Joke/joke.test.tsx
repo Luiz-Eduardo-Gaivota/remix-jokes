1c2282dc9ff7b17d303acd4d75867f775fc7ab3e
Luiz Eduardo Junior <92542169+Luiz-Eduardo-Gaivota@users.noreply.github.com>
2022-07-20 13:19:44 -0300

    feat: add test integrations

---
+++ app/components/Joke/joke.test.tsx
@@ -0,0 +1,40 @@
+import { screen } from "@testing-library/react";
+import { JokeDisplay } from "./index";
+import type { Joke } from "@prisma/client";
+import { render } from "../../utils/tests";
+
+describe("components/JokeDisplay", () => {
+  const MOCK_JOKE: Pick<Joke, "content" | "name"> = {
+    content:
+      "My first time using an elevator was an uplifting experience. The second time let me down.",
+    name: "Elevator",
+  };
+  const MOCK_TITLE = "Here's your hilarious joke:";
+  test("Should be render title", () => {
+    render(<JokeDisplay joke={MOCK_JOKE} isOwner={false} />);
+    expect(screen.getByText(MOCK_TITLE)).toBeInTheDocument();
+  });
+
+  test("Should be render name", () => {
+    render(<JokeDisplay joke={MOCK_JOKE} isOwner={false} />);
+    expect(screen.getByText(MOCK_JOKE.name + " Permalink")).toBeInTheDocument();
+  });
+
+  test("Should be render content", () => {
+    render(<JokeDisplay joke={MOCK_JOKE} isOwner={false} />);
+    expect(screen.getByText(MOCK_JOKE.content)).toBeInTheDocument();
+  });
+
+  test("Should be render delete button", () => {
+    render(<JokeDisplay joke={MOCK_JOKE} isOwner={true} />);
+    expect(screen.getByText("Delete")).toBeInTheDocument();
+  });
+
+  test("Should be render delete button with correctly style", () => {
+    render(<JokeDisplay joke={MOCK_JOKE} isOwner={true} />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.getByRole("button")).toHaveStyle({
+      backgroundColor: "hsl(48deg 100% 50%)",
+    });
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { installGlobals } = require("@remix-run/node");
+const { defaults } = require("jest-config");
+installGlobals();
+
+module.exports = {
+  roots: ["./app"],
+  setupFilesAfterEnv: ["./jest.setup.ts"],
+  testEnvironment: "jsdom",
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
+  testPathIgnorePatterns: ["node_modules/"],
+  transformIgnorePatterns: ["node_modules"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testMatch: ["**/*.test.(ts|tsx)"],
+  reporters: ["default"],
+  coverageReporters: ["json", "lcov", "text"],
+  moduleNameMapper: {
+    // Mocks out all these file formats when tests are run
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "identity-obj-proxy",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  collectCoverage: true,
+};
